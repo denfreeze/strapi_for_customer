2190b578aa4ccf63e8891258c9b1395f2763af1b
denfreeze <gm.denbrown2007@gmail.com>
2023-12-04 15:59:35 +0300

    Create license-key.js

---
+++ src/api/license-key/routes/license-key.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * license-key router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::license-key.license-key');
